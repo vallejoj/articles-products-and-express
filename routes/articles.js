6a210b0933d194f01fb1e28f934e76cf2502de6d
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-15 19:35:15 -1000

    this is the articles

--- routes/articles.js
+++ routes/articles.js
@@ -3,4 +3,22 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const router = express.Router();
 
+router.route('/')
+  .get((req, res) => {
+    console.log('this is artcles');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
