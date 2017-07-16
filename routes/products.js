62630160bc6e97f109e950aa8814031253f85e0a
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-15 19:36:05 -1000

    this the products

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,55 @@
 /*jshint esversion: 6 */
 const express = require('express');
-const exphbs = require('express-handlebars');
+//const exphbs = require('express-handlebars');
 const router = express.Router();
+const app = express();
+
+
+
+
+
+
+// /products
+//GET, POST
+
+router.route('/')
+  .get((req, res) => {
+    console.log('this is products');
+  });
+
+
+// /products/:id
+//GET, DELETE, PUT
+
+// /products/:id/edit
+//GET
+
+// /products/new
+//GET
+ module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
