be71129e94d3a6189946513a06af28e807aed862
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-15 18:31:52 -1000

    setting up hbs

--- views/server.js
+++ views/server.js
@@ -1,8 +1,9 @@
 /*jshint esversion 6*/
 
 const express = require('express');
-const exphbs = require('express-handlebars')
-
+const exphbs = require('express-handlebars');
+// const products = require('./routes/product.js');
+// const articles = require('./routes/articles.js');
 
 const app = express();
 const PORT = process.envPORT || 3000;
