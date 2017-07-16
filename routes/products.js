c7c9c8dffcaff214b98962e188980dfc5274e3f4
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-15 18:59:31 -1000

    connected product

---
+++ routes/products.js
@@ -0,0 +1,8 @@
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const router = express.Router();
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 const express = require('express');
 const exphbs = require('express-handlebars');
-// const products = require('./routes/product.js');
+
+var products = require('./routes/products.js');
 // const articles = require('./routes/articles.js');
 
 const app = express();
 const PORT = process.envPORT || 3000;
 
+
+app.use('products', products);
 
 const server = app.listen(PORT, () =>{
   console.log(`Running on ${PORT}`)
