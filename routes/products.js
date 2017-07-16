0015e0cb23acb92e8e3e0911618fd328216116a9
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-15 19:07:03 -1000

    this is the products

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,6 @@
-
+/*jshint esversion: 6 */
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const router = express.Router();
 
-
-module.exports = router
+module.exports = router;
