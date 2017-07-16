9e99cf435015988880b4a11609b95d631d1450ef
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-15 19:36:57 -1000

    this is the server

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 var products = require('./routes/products.js');
 var articles = require('./routes/articles.js');
-
 const app = express();
 const PORT = process.envPORT || 3000;
 
+//console.log(products);
+app.use('/products', products);
+app.use('/articles', articles);
 
-app.use('products', products);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const server = app.listen(PORT, () =>{
   console.log(`Running on ${PORT}`);
