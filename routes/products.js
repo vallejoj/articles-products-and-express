12c10354bf60274be70cfd2827eced19e19d2fd3
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-18 21:07:00 -1000

    Got put to work,

--- routes/products.js
+++ routes/products.js
@@ -5,23 +5,23 @@ const router = express.Router();
 const app = express();
 const bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-
-var products = [{ "id":"4","name": "MacBook Pro", "price": "1300", "inventory": "25" }];
+let id= 0
+var products = [];
 
 
 // '/produts' - POST
 router.route('/')
   .post(jsonParser, (req, res) => {
     if (typeof req.body === 'object'){
-    products.push({ id: (products.length + 1), name: req.body.name, price: req.body.price, inventory: req.body.inventory });//may have to figure out new way to create ID
-    console.log(req.body)
+    products.push({ "id":id++ , "name": req.body.name, "price": req.body.price, "inventory": req.body.inventory });//may have to figure out new way to create ID
+    console.log(products)
     res.redirect('/');
   } else {
     res.redirect('/new')
   }
   })
   .get((req, res) => {
-    res.render('products/index');
+    res.render('products/index', {products:products});
   });
 
 // '/products/:id - PUT
@@ -31,13 +31,17 @@ router.route(`/:id`)
     res.render('products/product');
 })
 .put(jsonParser, (req, res) => {
-  console.log("these are the "+ products[0].name)
+  console.log("these are the "+ req.body.name)
     products.forEach((item) => {
-      console.log("the "+ item.name)
-      if(req.body.id === item.id){
+      console.log("the "+ req.params.id)
+      console.log(item.id)
+      if (item.id == req.params.id ){
         item.name= req.body.name
-        // res.redirect('/')
-        console.log(item.name)
+        console.log("look at the"+ item.name)
+
+      }else{
+        console.log('poo')
+
       }
     })
 
