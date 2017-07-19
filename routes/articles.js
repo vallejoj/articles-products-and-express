1d9144a8bdffc5fd0540ae2091bb6aa687987ef1
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-19 12:03:24 -1000

    Working on making templates work for articles,
    For whatever reason I can not seem to get the Templates for articles to work. not sure what I am doing wrong.

--- routes/articles.js
+++ routes/articles.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const app = express();
 const bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-var titles = [];
+var articles = [];
 
 
 
@@ -17,12 +17,40 @@ router.route('/')
 })
 .post(jsonParser,(req, res) =>{  //use encodeURI()for creating encode URI titles
   if (typeof req.body === 'object'){
-  titles.push({ "title":req.body.title , "body": req.body.body, "Author": req.body.author});
-  console.log(titles)
+  articles.push({ "title":req.body.title , "body": req.body.body, "author": req.body.author, "urlTitle":encodeURI(req.body.title)});
+  console.log(articles)
   res.redirect('/');
 } else {
   res.redirect('/new')
 }
+});
+
+
+router.route(`/:title`)
+.get((req, res) => {
+    res.render('articles/article', {articles});
+})
+.put(jsonParser, (req, res) => {
+
+    articles.forEach((item) => {
+      if (item.urlTitle == req.params.urlTitle ){
+        item.title= req.body.title
+      }else{
+
+      }
+    })
+
+})
+  .delete((req, res) => {
+    products.forEach((item) => {
+
+      if (item.id == req.params.id ){
+          var findIDToDelete = articles.indexOf(item)
+          articles.splice(findIDToDelete,1);
+      }else{
+
+      }
+    })
 });
 
 
