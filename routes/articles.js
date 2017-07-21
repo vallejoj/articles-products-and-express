506dff3aae263f61c61f8887268ea680e78725d5
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-20 17:23:04 -1000

    adding comments to help me

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,4 @@
 /*jshint esversion: 6 */
-//Joshua Lee
 const express = require('express');
 const router = express.Router();
 const app = express();
@@ -10,45 +9,39 @@ var articles = [];
 
 
 router.route('/')
-.get((req,res)=>{
-  res.render('articles/index')
-})
-.post(jsonParser,(req, res) =>{  //use encodeURI()for creating encode URI titles
-  if (typeof req.body === 'object'){
-  articles.push({ "title":req.body.title , "body": req.body.body, "author": req.body.author, "urlTitle":encodeURI(req.body.title)});
-  console.log(articles)
-  res.redirect('/');
-} else {
-  res.redirect('/new')
-}
+  .get((req,res)=>{ //<--renders HTML with all articles
+    res.render('articles/index');
+  })
+  .post(jsonParser,(req, res) =>{ //<--creates new article; use encodeURI()for creating encode URI titles
+    if (typeof req.body === 'object'){
+      articles.push({ "title":req.body.title , "body": req.body.body, "author": req.body.author, "urlTitle":encodeURI(req.body.title)});
+      res.redirect('/');
+      } else {
+      res.redirect('/new');
+  }
 });
-
 
 router.route(`/:title`)
-.get((req, res) => {
+  .get((req, res) => { //<--renders HTML generated from templates displaying article info by title
     res.render('articles/article', {articles});
-})
-.put(jsonParser, (req, res) => {
-
+  })
+  .put(jsonParser, (req, res) => { //<--update article infromation
     articles.forEach((item) => {
       if (item.urlTitle == req.params.urlTitle ){
-        item.title= req.body.title
-      }else{
-
+        item.title= req.body.title;
+        }else{
       }
-    })
-
-})
-  .delete((req, res) => {
+    });
+  })
+  .delete((req, res) => { //<--removes an articel by title
     products.forEach((item) => {
-
       if (item.id == req.params.id ){
-          var findIDToDelete = articles.indexOf(item)
-          articles.splice(findIDToDelete,1);
+        var findIDToDelete = articles.indexOf(item);
+        articles.splice(findIDToDelete,1);
       }else{
-
-      }
-    })
+    }
+  });
 });
 
+//exporting the router module
 module.exports = router;
