49d56bf0136443f37318733f546fbd2c5894c857
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-20 17:05:35 -1000

    fixing syntax

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,10 @@
 /*jshint esversion: 6 */
-//Joshua Lee
 const express = require('express');
 const router = express.Router();
 const app = express();
 
 
-let id = 0
+let id = 0;
 var products = [];
 
 router.route('/')
@@ -15,25 +14,21 @@ router.route('/')
     });
   })
   .post((req, res) => {
-    //  let matches = req.body.name.match(/\d+/g);
-console.log(req.body);
     if (typeof req.body === 'object') {
       products.push({
         "id": id++,
         "name": req.body.name,
         "price": req.body.price,
         "inventory": req.body.inventory
-      }); //may have to figure out new way to create ID
-      console.log(products)
+      });
+      console.log(products);
       res.redirect('/products/product');
-    } else {
-      res.redirect('/products/new')
+      } else {
+      res.redirect('/products/new');
     }
   });
 
 
-
-// '/products/:id/edit - GET
 router.get('/:id/edit', (req, res) => {
   res.render('products/edit', {
     products: products
@@ -46,7 +41,6 @@ router.get('/new', (req, res) => {
   });
 });
 
-
 router.route(`/:id`)
   .get((req, res) => {
     res.render('products/product', {
@@ -56,25 +50,23 @@ router.route(`/:id`)
   .put((req, res) => {
     products.forEach((item) => {
       if (item.id == req.params.id) {
-        item.name = req.body.name
+        item.name = req.body.name;
         res.redirect('/products/product');
-        console.log(item)
-      } else {
-        res.redirect('/new')
+        console.log(item);
+        } else {
+        res.redirect('/new');
       }
-    })
+    });
   })
   .delete((req, res) => {
     products.forEach((item) => {
-
       if (item.id == req.params.id) {
-        var findIDToDelete = products.indexOf(item)
+        var findIDToDelete = products.indexOf(item);
         products.splice(findIDToDelete, 1);
-      } else {
-
+        } else {
       }
-    })
+    });
   });
 
-
+//exporting the router module
 module.exports = router;
