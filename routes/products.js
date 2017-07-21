849d8106c4ed0ed20e37d1d333aa2b175ff37c3c
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-20 20:26:24 -1000

    Added method override

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 const bodyParser = require('body-parser');
+var methodOverride = require('method-override')
 var jsonParser = bodyParser.json();
 
 
