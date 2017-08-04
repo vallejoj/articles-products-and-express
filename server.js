a852ddec102392846ffeda829889a3da0289eb6d
Joshua Lee <jdl93@nau.edu>
2017-08-03 21:54:35 -1000

    stuff

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.use('/css', express.static('css'));
-app.use(bp.urlencoded())
+app.use(bp.urlencoded());
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
   }
 }));
 
