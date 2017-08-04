61cd40d020b9c3bdddb27742ddf301604918cdf4
Joshua Lee <jdl93@nau.edu>
2017-08-03 21:54:15 -1000

    stuff

--- routes/products.js
+++ routes/products.js
@@ -4,26 +4,51 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 const bodyParser = require('body-parser');
-const db = require ('../db')
+//const db = require ('../db');
 // const productsDB = require('./db/products.js')
 var jsonParser = bodyParser.json();
+
+
+const pgp = require('pg-promise')();
+
+const { DATABASE, USER, PASSWORD} = require('../config/config');
+
+const connectionOptions = {
+  host:'localhost',
+  port: 5432,
+  database: DATABASE,
+  user: USER,
+  password: PASSWORD
+};
+const db = pgp(connectionOptions);
+
+function getAllProducts(){
+  return db.query('SELECT * FROM products_table');
+}
+
+
 
 
 router.route('/')
   //WORKING
   .get((req, res) => { //<--renders HTML with all products
-    res.render('products/index', {
-      products: products
-    });
+    getAllProducts()
+    .then((products) => {
+      console.log(products);
+      res.render('products/index', {products});
+    })
+    .catch((err) => {
+        console.log(err);
+      });
+    // res.render('products/index', {
+    //   products: products
+    // });
   })
   .post(jsonParser,(req, res) => { //<--creates a new product
-console.log('this is req.body',req.body)
+    console.log('this is req.body',req.body);
     if (typeof req.body === 'object') { //<--need to change this to validate data that is a new product
-///insert data
-
-      ;
-      console.log(products)
       res.redirect('/products');
+      return db.none(`INSERT INTO products_table VALUES (default, ${req.body.name}, ${req.body.price}, ${req.body.inventory}`);
       } else {
       res.redirect('/products/new');
     }
@@ -37,7 +62,7 @@ router.route('/:id/edit')
   });
 })
   .put((req, res) => { //<--edits a product
-    console.log('PUT recieved')
+    console.log('PUT recieved');
     products.forEach((item) => {
       item.name = req.body.name;
       res.redirect('/products');
@@ -54,7 +79,7 @@ router.get('/new', (req, res) => { //<--renders HTML generated from templates (f
 
   router.route('/delete')
   .get((req, res) => {
-    console.log('rendering???')//<--renders HTML generated from templates (form to create product)
+    console.log('rendering???');//<--renders HTML generated from templates (form to create product)
     res.render('products/delete', {
       products: products
     });
@@ -63,8 +88,8 @@ router.get('/new', (req, res) => { //<--renders HTML generated from templates (f
     products.forEach((item) => {
         var findIDToDelete = products.indexOf(item);
         products.splice(findIDToDelete, 1);
-        console.log('delete')
-    })
+        console.log('delete');
+    });
   });
 
 
@@ -72,9 +97,9 @@ router.get('/new', (req, res) => { //<--renders HTML generated from templates (f
 
 router.route(`/:id`)
   .get((req, res) => { //<--renders HTML generated from templates (display product by ID)
-console.log('you got me here')
+console.log('you got me here');
     products.forEach((item) => {
-      console.log(item.id + "and" + req.params.id )
+      console.log(item.id + "and" + req.params.id );
       if (req.params.id === item.id){
         console.log('this is the item.id: ', item);
         res.render('products/product', {
@@ -84,7 +109,7 @@ console.log('you got me here')
         //code block
       }
     });
-  })
+  });
 
 
 //exporting the router module
